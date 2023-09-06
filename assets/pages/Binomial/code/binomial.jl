036d7e7d78a147c28a07391428650d8454a2b04e
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2023-09-06 21:53:48 +0000

    Deploying to gh-pages from  @ dc844fdc3d815be10c9026c24aad96bf7b4a4d0f 🚀

--- assets/pages/Binomial/code/binomial.jl
+++ assets/pages/Binomial/code/binomial.jl
@@ -1,4 +1,4 @@
 # This file was generated, do not modify it. # hide
 using Distributions, StatsPlots
-plot(Binomial(10, 0.6), xlabel="number of trials", ylabel="probability", label=nothing, xticks=1:1:10)
+plot(Binomial(10, 0.6), xlabel="number of trials", ylabel="probability", label=nothing, xticks=0:1:10)
 savefig(joinpath(@OUTPUT, "binomial_10_06.svg"))
