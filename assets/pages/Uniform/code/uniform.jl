5287b18699d6b918f1b5eec18942839848c75b20
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2023-09-06 00:44:15 +0000

    Deploying to gh-pages from  @ 90e7a6185eb4b57da1bb7a27c6d19417ce7df08d 🚀

--- assets/pages/Uniform/code/uniform.jl
+++ assets/pages/Uniform/code/uniform.jl
@@ -1,4 +1,4 @@
 # This file was generated, do not modify it. # hide
 using Distributions, StatsPlots
-plot(Uniform(1, 3), label="PDF")
+plot(Uniform(1, 3), ylims=(0,1), label="PDF")
 savefig(joinpath(@OUTPUT, "uniform.svg"))
