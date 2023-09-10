9d2feb5d838f9d0ec1e4321f193049289c7ff1da
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2023-09-10 05:42:06 +0000

    Deploying to gh-pages from  @ 8d66a1e6a459fafbb28e0cf8984bf69d7f0b4930 🚀

--- assets/pages/DistributionFitting/code/_ceval_3.jl
+++ assets/pages/DistributionFitting/code/_ceval_3.jl
@@ -1,4 +1,4 @@
 # This file was generated, do not modify it. # hide
-histogram(outcomes - df.spread, label="outcome - point spread")
+histogram(outcomes - df.spread, label="outcome - point spread", ylabel="#")
 savefig(joinpath(@OUTPUT, "football.svg"))
 nothing
