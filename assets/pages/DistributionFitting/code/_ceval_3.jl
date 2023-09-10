f075e9016fc8acfebde4308051aca7ac0f4cfdf7
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2023-09-10 07:10:15 +0000

    Deploying to gh-pages from  @ d6262d6f6122281022ad2e54e5d2cf048f204cfd 🚀

--- assets/pages/DistributionFitting/code/_ceval_3.jl
+++ assets/pages/DistributionFitting/code/_ceval_3.jl
@@ -1,4 +1,4 @@
 # This file was generated, do not modify it. # hide
-histogram(outcomes - df.spread, label="outcome - point spread", ylabel="#", guidefontsize=20)
+histogram(outcomes - df.spread, label="outcome - point spread", ylabel="#", guidefontsize=30)
 savefig(joinpath(@OUTPUT, "football.svg"))
 nothing
--- assets/pages/DistributionFitting/code/_ceval_5.jl
+++ assets/pages/DistributionFitting/code/_ceval_5.jl
@@ -1,5 +1,5 @@
 # This file was generated, do not modify it. # hide
-histogram(outcomes - df.spread, normalize=:pdf, label="outcome - point spread", ylabel="#", guidefontsize=20)
+histogram(outcomes - df.spread, normalize=:pdf, label="outcome - point spread", ylabel="#", guidefontsize=30)
 plot!(fitted, label="Normal PDF")
 savefig(joinpath(@OUTPUT, "football_fitted.svg"))
 nothing
