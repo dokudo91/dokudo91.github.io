12cb3b756400a1974ce415fa1c451e2a7aefc558
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2023-09-13 08:59:02 +0000

    Deploying to gh-pages from  @ 9681b8811e9aefdf52370562ded78665532a8156 🚀

--- assets/pages/TransformingProbabilityDistributions/code/_ceval_5.jl
+++ assets/pages/TransformingProbabilityDistributions/code/_ceval_5.jl
@@ -1,4 +1,4 @@
 # This file was generated, do not modify it. # hide
-contour(-0.5:0.01:0.5, -0.5:0.01:0.5, (x, y) -> pv([x, y]), size=(500, 400))
+contour(-0.5:0.01:0.5, -0.5:0.01:0.5, (x, y) -> pv([x, y]), size=(500, 400), yrotation=90)
 savefig(joinpath(@OUTPUT, "pv.svg"))
 nothing
