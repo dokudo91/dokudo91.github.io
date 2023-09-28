8ea199dd53e5c3ac994826a5f7ea1f7f2f19193b
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2023-09-28 18:53:23 +0000

    Deploying to gh-pages from  @ b0f15d419988fa49df0009b76dce0539b9d6cee8 🚀

---
+++ assets/pages/BDA3_Exercises2_11_2/code/_ceval_1.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+PrC1TT=0.5*0.4^2/(0.5*0.4^2+0.5*0.6^2)
+PrC2TT=1-PrC1TT
+PrC1TT, PrC2TT
---
+++ assets/pages/BDA3_Exercises2_11_2/code/_ceval_2.jl
@@ -0,0 +1,2 @@
+# This file was generated, do not modify it. # hide
+1/0.6 * PrC1TT + 1/0.4 * PrC2TT
